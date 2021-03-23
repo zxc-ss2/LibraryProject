d1bcab6efdaad82e12ae7ba0fc788ed64689d523
Алексей Скочков <skochkov.aleksey@yandex.ru>
2021-03-23 19:57:39 +0500

    Добавьте файлы проекта.

---
+++ Views/AuthorizationPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LibraryProject.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationPage.xaml
+    /// </summary>
+    public partial class AuthorizationPage : Page
+    {
+        public AuthorizationPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void SignUpBtnClick(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new RegistrationPage());
+        }
+
+        private void LoginBtnClick(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new AuthorizationPage());
+        }
+    }
+}
